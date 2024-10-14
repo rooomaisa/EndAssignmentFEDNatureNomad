978521bae7a9c44a71727de83babb50eb9a5a43f
rooomaisa <romyvandam334@gmail.com>
2024-10-14 16:11:33 +0200

    home page bacis done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import {Link} from "react-router-dom";
+import './Home.css'
+
 
 function Home() {
     return (
-        <div className={'page-container'}>
-            <h1>Home</h1>
+        <section className={'page-container'}>
+        <div className={`inner-content-container__home-content`}>
+            <h1>Welcome to NatureNomad</h1>
+            <h2>"Find Your Perfect Wilderness Escape" </h2>
+            <Link to={`/search`}>
+            <button className={`homepage-btn btn`}>Start your search</button>
+            </Link>
         </div>
+        </section>
     );
 }
 
